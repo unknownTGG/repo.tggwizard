00fe4d6e1fd3832dfb06cbc924792949da5ae982
unknownTGG <48967944+unknownTGG@users.noreply.github.com>
2019-07-28 20:20:05 -0400

    Update uservar.py

--- plugin.program.tgg/uservar.py
+++ plugin.program.tgg/uservar.py
@@ -103,9 +103,9 @@ AUTOINSTALL    = 'Yes'
 # Addon ID for the repository
 REPOID         = 'repository.tgg'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML   = 'https://raw.githubusercontent.com/drinfernoo/repository.aftermath/master/zips/addons.xml'
+REPOADDONXML   = 'https://raw.githubusercontent.com/unknownTGG/unknownTGG.github.io/master/zips/addons.xml'
 # Url to folder zip is located in
-REPOZIPURL     = 'https://raw.githubusercontent.com/drinfernoo/repository.aftermath/master/zips/repository.aftermath/'
+REPOZIPURL     = 'https://raw.githubusercontent.com//unknownTGG/unknownTGG.github.io//master/zips/repository.tgg/'
 #########################################################
 
 #########################################################
